10246e8cf5e1207dd21abdb404b5ca7bab322a8c
Freehaha <freehaha@gmail.com>
2020-09-25 19:36:28 +1000

    planck update

--- keyboards/planck/keymaps/freehaha/keymap.c
+++ keyboards/planck/keymaps/freehaha/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_EXT] = LAYOUT_planck_grid(
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
+		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
 		_______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
 		),
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_EXTR] = LAYOUT_planck_grid(
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-		KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 		),
@@ -137,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = LAYOUT_planck_grid( /* GAME MODE */
 		KC_GRAVE,   GEXT2,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_NO,   KC_UP,    KC_NO,   KC_NO,     KC_HOME,
 		KC_LSFT,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,   KC_LEFT, KC_DOWN,  KC_RIGHT,KC_P,      KC_END,
-		KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_NO,   KC_NO,    KC_NO,   KC_NO,     KC_NO,
+		KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_NO,   KC_NO,    KC_NO,   KC_NO,     KC_ENT,
 		GEXT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_SPC,  KC_NO,    KC_NO,   KC_NO,     GAME
 		),
 
