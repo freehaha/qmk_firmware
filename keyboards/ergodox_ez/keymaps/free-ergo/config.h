ccfa4c664fbdea1719aacbbd71ef750ebd83c7b3
Freehaha <freehaha@gmail.com>
2021-06-02 00:21:01 +1000

    ergodox setup

---
+++ keyboards/ergodox_ez/keymaps/free-ergo/config.h
@@ -0,0 +1,9 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+// #define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"rDPY7/qQJd4"
+// #define PERMISSIVE_HOLD
+// #define TAPPING_TERM 100
