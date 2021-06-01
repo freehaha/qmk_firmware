ada23a378849ac17e76497c175be728301c5f158
John Sun <john.sun@bookipi.com>
2021-06-02 08:50:17 +1000

    individual tap-hold interrupt setting

--- keyboards/planck/keymaps/freehaha/keymap.c
+++ keyboards/planck/keymaps/freehaha/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,     KC_L,    KC_U,     KC_Y,    KC_SCLN, KC_BSPC,
     KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,     KC_N,    KC_E,     KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT, ZCTL,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,     KC_M,    KC_COMM,  KC_DOT,  SLCTL,   SENT,
+    KC_LSFT, KC_TRNS, KC_X,    KC_C,    KC_V,    KC_B,    KC_K,     KC_M,    KC_COMM,  KC_DOT,  KC_TRNS, SENT,
     KC_ESC,  MO(_FN), KC_LGUI, KC_LALT, CENT,    EXT,     EXTR,     KC_SPC,  KC_GRAVE, KC_DEL,  MO(_FN), MO(_FN)
 ),
 
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_ESC,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PIPE,    S(KC_NUBS), KC_HOME, KC_END,  _______,
-		_______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
+		_______, _______, _______, _______, _______, _______, _______, KC_BSPC,    _______,    _______, _______, _______
 		),
 
 /* EXTR
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+		_______, _______, _______, _______, _______, _______, _______, KC_BSPC, _______, _______, _______, _______
 		),
 
 [_FN] = LAYOUT_planck_grid( /* FUNCTION */
@@ -162,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______,  _______, _______, KC_7,    KC_8,    KC_9,    KC_NO,   KC_NO,
 		KC_F10,  _______, _______, _______, KC_TAB,   _______, _______, KC_4,    KC_5,    KC_6,    KC_NO,   KC_NO,
 		KC_NO,   _______, _______, _______, _______,  _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,
-		_______, _______, _______, _______, _______,  _______, _______, KC_0,    _______, KC_DOT,  _______, TG(_NUMBERS)
+		_______, _______, _______, _______, _______,  _______, _______, KC_0,    _______, KC_DOT,  TG(_NUMBERS), TG(_NUMBERS)
 		),
 
 /* Adjust (Lower + Raise)
@@ -266,6 +266,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	return true;
 }
 
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case CENT:
+        case SENT:
+            return false;
+        default:
+            return true;
+    }
+}
 
 bool music_mask_user(uint16_t keycode) {
   switch (keycode) {
