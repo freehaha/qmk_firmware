b435dad81de2c98174d70b5842662b0992ebd54c
Freehaha <freehaha@gmail.com>
2021-06-05 15:48:58 +1000

    map ralt to thumb clusters

--- keyboards/ergodox_ez/keymaps/free-ergo/keymap.c
+++ keyboards/ergodox_ez/keymaps/free-ergo/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,         WEBUSB_PAIR,    KC_LALT,        KC_LGUI,        CENT,                                                                                                           KC_SPACE,       KC_GRAVE,       KC_LBRACKET,    MO(_FN),          KC_ESC,
                                                                                                     LALT_T(KC_APPLICATION), KC_LGUI,        KC_LALT,        LCTL_T(KC_ESCAPE),
                                                                                                                       KC_HOME,        KC_PGUP,
-                                                                                    SYMBL_ESC, KC_TAB, KC_END,         KC_PGDOWN,      KC_BSPC,         SYMBR_ENT
+                                                                                    SYMBL_ESC, KC_TAB, KC_END,         KC_PGDOWN,      KC_LALT,         SYMBR_ENT
   ),
   [_CMK] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
