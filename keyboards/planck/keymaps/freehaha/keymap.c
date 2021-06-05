eea5bbb779cff668e52301874b2c0a26b00a1598
Freehaha <freehaha@gmail.com>
2021-06-05 23:09:41 +1000

    www

--- keyboards/planck/keymaps/freehaha/keymap.c
+++ keyboards/planck/keymaps/freehaha/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,     KC_L,    KC_U,     KC_Y,    KC_SCLN, KC_BSPC,
     KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,     KC_N,    KC_E,     KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_TRNS, KC_X,    KC_C,    KC_V,    KC_B,    KC_K,     KC_M,    KC_COMM,  KC_DOT,  KC_TRNS, SENT,
-    KC_ESC,  MO(_FN), KC_LGUI, KC_LALT, CENT,    EXT,     EXTR,     KC_SPC,  KC_GRAVE, KC_DEL,  MO(_FN), MO(_FN)
+    KC_ESC,  MO(_FN), KC_LALT, KC_LGUI, CENT,    EXT,     EXTR,     KC_SPC,  KC_GRAVE, KC_DEL,  MO(_FN), MO(_FN)
 ),
 
 /* EXT
