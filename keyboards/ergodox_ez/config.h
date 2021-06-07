6e7b0fe998c685a7963c843f9d4b2271c19644f0
Freehaha <freehaha@gmail.com>
2021-06-07 22:07:05 +1000

    adjust debounce

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_MAX_SPEED      12
 #define MOUSEKEY_WHEEL_DELAY 0
 
-#define DEBOUNCE 7
+#define DEBOUNCE 5
 
 #define TAPPING_TOGGLE  1
 
